52b8df2aafb9836679d8f2e83385d090f8080de5
Daniel Teres <danielteres@correo.ugr.es>
2024-11-28 23:45:53 +0100

    Started to develope the embeds with the result of randomization

--- src/Game.py
+++ src/Game.py
@@ -8,6 +8,13 @@ from riotwatcher import LolWatcher, ApiError
 from dotenv import load_dotenv
 
 # URL to json that contain champs https://ddragon.leagueoflegends.com/cdn/14.23.1/data/en_US/champion.json
+colors_runes = {
+    "Domination" : (215,65,67),
+    "Precision" : (206,174,123),
+    "Inspiration" : (66,167,172),
+    "Resolve" : (157,214,132),
+    "Sorcery" : (156,167,246)
+}
 
 class Game(commands.Cog):
     def __init__(self):
@@ -39,12 +46,12 @@ class Game(commands.Cog):
 
 
         # select for each one randomly champ, runes and summoners
-        with open(os.path.join(os.getcwd(), "data", "champions_parsed.json")) as f:
+        with open(os.path.join(os.getcwd(), "data", "champions_red.json")) as f:
             champs = json.load(f)
+
             for player in participants:
                 player_slection = dict()
-                champ_name, champ_image = random.choice(list(champs.items()))
-                player_slection[champ_name] = champ_image
+                
                 runes_selected = False
 
                 for key in self.data_assets:
@@ -52,32 +59,91 @@ class Game(commands.Cog):
                         break
 
                     limit = 4
-                    if key == "item":
-                        limit = 3
-                    elif key == "summoner":
+                    path = os.path.join(os.getcwd(), "assets", "game_images")
+                    
+                    if key == "1champion":
+                        champ_name, champ_image = random.choice(list(champs.items()))
+                        player_slection[champ_name] = champ_image
+                    elif key == "2summoner":
                         limit = 2
+                        path = os.path.join(path, "2summoner")
+                    elif key == "3item":
+                        limit = 3
+                        path = os.path.join(path, "3item")
                     else:
                         rune_selection = [rune for rune in self.data_assets 
-                                          if rune not in ["item", "summoner"]]
+                                          if rune not in ["3item", "2summoner"]]
                         key = random.choice(rune_selection)
                         runes_selected = True
+                        path = path = os.path.join(path, "4runes", key)
 
+                    path = os.path.relpath(path, os.getcwd())
                     key_values = []
+
                     while len(key_values) < limit:
-                        value = random.choice(self.data_assets[key])
+                        value = os.path.join(path, random.choice(
+                                            self.data_assets[key]))
 
                         if value not in key_values:
                             key_values.append(value) 
-                    
+                    #! CHECK THAT PLAYER_SELECTION HAVE ALL VALUES
                     player_slection[key] = key_values
                 
                 print(f"{player.name} has to choose the following:\n{player_slection}")
                 
 
-        # Add all of them to an embed message
+                # Add all of them to an embed message
+                await self.__embed_msg(ctx, player.name, player_slection)
 
         # send that embed through channel
 
+
+    async def __embed_msg(self, ctx, user : str, player_selection : dict):
+        iteration = 0
+        image_file : discord.File = None
+        category = ["champion -->", "items -->", "summoners -->", "runes -->"]
+        pool_embeds = []
+        files_images = []
+
+        for key, values in player_selection.items():
+            embed = discord.Embed(
+                title=f"Selection for {user}",
+                color=discord.Colour.from_rgb(*colors_runes[list(player_selection)[-1]])
+            )
+            embed.add_field(name=category[iteration], value="")
+            
+
+            try:
+                if not isinstance(values, list):
+                    values = [values]
+                    values[0] = os.path.abspath(values[0])
+
+                for i in range (0, len(values)):
+                    path_image = values[i]
+                    image_file = discord.File(path_image, filename=path_image.split("\\")[-1])
+                    #! CHECK ALL IMAGES ARE INCLUDE IN THE ORDER NEEDED FOR THE FINAL LOOP
+                    files_images.append(image_file)
+                    #embed.set_image(url=f"attachment://{image_file.filename}")
+                    
+                pool_embeds.append(embed)
+                iteration += 1
+
+            except FileNotFoundError:
+                raise FileNotFoundError(f"route of file {path_image} wasn't found")
+        
+        init = 0
+        fin = 1
+        aumento = 2
+        #! CHECK THAT IS ONLY GETTING THE NECESARY IMAGES ON EACH ITERATION
+        for emb in pool_embeds:
+            if init == 0:
+                await ctx.send(embed=emb, file=files_images[0])
+            else:
+                await ctx.send(embed=emb, files=files_images[init:fin])
+            
+            init = fin
+            fin += aumento
+            aumento += 1
 
 
 
@@ -98,6 +164,7 @@ class Game(commands.Cog):
                         
                 return files_subdir
 
+
     async def __get_num_champs(self, ctx) -> list[discord.Member]:
         try:
             participants = ctx.author.voice.channel.members
@@ -106,6 +173,7 @@ class Game(commands.Cog):
             raise Exception("You have to be connected to a voice channel")
         else:
             return participants
+    
     
     def __read_num_champions(self) -> int:
         with open(os.path.join(os.getcwd(), "data", "champions_parsed.json")) as f:
